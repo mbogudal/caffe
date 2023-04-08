a15af7f5e9cd9d9920a8f3f6c33641daa7b09b0d
Mikolaj Bogudal <mbogudal@gmail.com>
2023-04-08 18:19:45 +0200

    fixed function call

--- cmake/Modules/FindvecLib.cmake
+++ cmake/Modules/FindvecLib.cmake
@@ -13,12 +13,10 @@ endif()
 set(__veclib_include_suffix "Frameworks/vecLib.framework/Versions/Current/Headers")
 
 exec_program(xcode-select ARGS -print-path OUTPUT_VARIABLE CMAKE_XCODE_DEVELOPER_DIR)
-find_path(vecLib_INCLUDE_DIR vecLib.h
-          DOC "vecLib include directory"
-          PATHS /System/Library/Frameworks/Accelerate.framework/Versions/Current/${__veclib_include_suffix}
-                /System/Library/${__veclib_include_suffix}
-                ${CMAKE_XCODE_DEVELOPER_DIR}/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/System/Library/Frameworks/Accelerate.framework/Versions/Current/Frameworks/vecLib.framework/Headers/
-          NO_DEFAULT_PATH)
+find_path(vecLib_INCLUDE_DIR cblas.h
+      DOC "vecLib include directory"
+      PATHS /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/System/Library/Frameworks/Accelerate.framework/Versions/Current/Frameworks/vecLib.framework/Headers/
+      NO_DEFAULT_PATH)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(vecLib DEFAULT_MSG vecLib_INCLUDE_DIR)
